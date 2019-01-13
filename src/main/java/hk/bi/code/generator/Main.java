8bde86315ef9b3ad1d40b9362e456f2f4a46b7dd
fisherYuTou <yuyanwu4798@qq.com>
2019-01-13 13:00:33 +0800

    asdfasdf

--- src/main/java/hk/bi/code/generator/Main.java
+++ src/main/java/hk/bi/code/generator/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    //可配置1111132323233
+    //可配置2
     private static String beanBasePath = null;
     private static String mappingBasePath = null;
     private static String mapperBasePath = null;
